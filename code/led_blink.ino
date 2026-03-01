0c798cb5f039239f57920a764b651093c44b54ae
Chaitali10-10 <chaitalipatil1459@gmail.com>
2026-03-01 20:05:07 +0530

    Add Arduino LED blinking code

---
+++ code/led_blink.ino
@@ -0,0 +1,17 @@
+// LED Blink Program
+// Author: Chaitali Patil
+// Board: Arduino Uno
+
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
